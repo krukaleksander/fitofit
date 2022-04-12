d72650570d5f0f416cd8acb13bc1cdb2858ad192
Aleksander Kruk <kruk.aleksander@hotmail.com>
2022-04-12 15:15:17 +0200

    feat: add validation to register dto

--- packages/server/src/auth/dto/RegisterCredentials.dto.ts
+++ packages/server/src/auth/dto/RegisterCredentials.dto.ts
@@ -1,5 +1,14 @@
+import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
+
 export class RegisterCredentialsDto {
+  @IsString()
+  @IsNotEmpty()
   login: string;
+  @IsString()
+  @IsNotEmpty()
+  @IsEmail()
   email: string;
+  @IsString()
+  @IsNotEmpty()
   password: string;
 }
