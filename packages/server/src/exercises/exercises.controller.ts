3e55b439f519e2d3865cf5d42f5350e750ec1216
Aleksander Kruk <kruk.aleksander@hotmail.com>
2022-04-12 10:05:10 +0200

    feat: create post endpoint exercise/new

--- packages/server/src/exercises/exercises.controller.ts
+++ packages/server/src/exercises/exercises.controller.ts
@@ -1,13 +1,17 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { ExercisesService } from './exercises.service';
 import { ExerciseFromServerDto } from './dto/ExerciseFromServer.dto';
 
 @Controller('exercises')
 export class ExercisesController {
-  constructor(private readonly appService: ExercisesService) {}
+  constructor(private readonly exerciseService: ExercisesService) {}
 
   @Get('/')
   getExercises(): ExerciseFromServerDto[] {
-    return this.appService.getExercises();
+    return this.exerciseService.getExercises();
+  }
+  @Post('new')
+  addAcivity(@Body() activity) {
+    return this.exerciseService.addActivity(activity);
   }
 }
--- packages/server/src/exercises/exercises.service.ts
+++ packages/server/src/exercises/exercises.service.ts
@@ -1,8 +1,14 @@
 import { Injectable } from '@nestjs/common';
 import { db as MOCKED_RESPONSE } from '../../tempdb/db';
+import { ExerciseFromServerDto } from './dto/ExerciseFromServer.dto';
+import { ActivityDto } from './dto/Activity.dto';
 @Injectable()
 export class ExercisesService {
-  getExercises() {
+  getExercises(): ExerciseFromServerDto[] {
     return MOCKED_RESPONSE;
+  }
+
+  addActivity(activity: ActivityDto) {
+    return { status: 200, msg: 'Dodano zadanie', activity };
   }
 }
