6c80df2761646ab644dbd29d92e79b82d0e12869
Aleksander Kruk <kruk.aleksander@hotmail.com>
2022-04-13 14:33:00 +0200

    ref: remove typeOrm from database.module

--- packages/server/src/database/database.module.ts
+++ packages/server/src/database/database.module.ts
@@ -1,20 +1,6 @@
 import { Module } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { ActivityEntity } from '../exercises/activity.entity';
-import { AuthEntity } from '../auth/auth.entity';
 
 @Module({
-  imports: [
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: 'localhost',
-      port: 5434,
-      username: 'postgres',
-      password: 'myPassword123',
-      database: 'fitofit',
-      entities: [ActivityEntity, AuthEntity],
-      synchronize: true,
-    }),
-  ],
+  imports: [],
 })
 export class DatabaseModule {}
