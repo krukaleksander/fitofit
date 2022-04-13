06aa9ce4fdd00115abfccb6b2d2c5c66498e2170
Aleksander Kruk <kruk.aleksander@hotmail.com>
2022-04-13 09:31:18 +0200

    feat: implement hashing password

--- packages/server/src/auth/auth.service.ts
+++ packages/server/src/auth/auth.service.ts
@@ -1,9 +1,16 @@
 import { Injectable } from '@nestjs/common';
 import { RegisterCredentialsDto } from './dto/RegisterCredentials.dto';
-
+import * as argon from 'argon2';
 @Injectable()
 export class AuthService {
-  registerUser(credentials: RegisterCredentialsDto) {
-    return { status: 200, credentials };
+  async registerUser({ email, login, password }: RegisterCredentialsDto) {
+    return {
+      status: 200,
+      credentials: {
+        login: login,
+        password: await argon.hash(password),
+        email: email,
+      },
+    };
   }
 }
