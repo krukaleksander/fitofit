31929847186c9010c6cbfd59fba72ac20539d526
Aleksander Kruk <kruk.aleksander@hotmail.com>
2022-04-13 14:06:18 +0200

    feat: add ormconfig.ts

---
+++ packages/server/src/database/ormconfig.ts
@@ -0,0 +1,46 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+import { AuthEntity } from '../auth/auth.entity';
+import { ActivityEntity } from '../exercises/activity.entity';
+
+module.exports = [
+  {
+    name: 'default', //for all environments
+    type: 'postgres',
+    url: process.env.DATABASE_URL,
+    schema: 'public',
+
+    synchronize: false,
+    migrationsRun: true,
+
+    logging: process.env.DATABASE_LOGGING === 'true',
+
+    autoLoadEntities: true,
+
+    entities: [AuthEntity, ActivityEntity],
+    migrationsTableName: 'migrations',
+
+    cli: {
+      migrationsDir: './src/migrations',
+    },
+  },
+  {
+    name: 'test',
+    type: 'postgres',
+    schema: 'public',
+
+    synchronize: false,
+    migrationsRun: true,
+
+    logging: process.env.DATABASE_LOGGING === 'true',
+
+    autoLoadEntities: true,
+
+    entities: ['./packages/backend/src/**/*.entity.ts'], // tests run on TS directly
+    migrations: ['./packages/backend/src/**/migrations/*.ts'],
+    migrationsTableName: 'migrations',
+
+    cli: {
+      migrationsDir: './packages/backend/src/migrations',
+    },
+  },
+];
