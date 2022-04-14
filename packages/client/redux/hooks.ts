e465f1c2ebdd0c50e0df7eb919409bc95ff4df23
hub33k <hub33k@yahoo.com>
2022-04-14 18:26:49 +0200

    refactor: remove redux code

--- packages/client/redux/hooks.ts
+++
@@ -1,9 +0,0 @@
-import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux';
-import { ThunkDispatch, AnyAction } from '@reduxjs/toolkit';
-import type { RootState, AppDispatch } from '~/redux/store';
-
-// Use throughout your app instead of plain `useDispatch` and `useSelector`
-export const useAppDispatch = (): ThunkDispatch<RootState, any, AnyAction> =>
-  useDispatch<AppDispatch>();
-
-export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
