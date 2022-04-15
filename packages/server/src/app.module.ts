d96a0b574fcee334f3542f7293dff3ca4b0f529d
Aleksander Kruk <kruk.aleksander@hotmail.com>
2022-04-15 09:43:04 +0200

    ref: app module

--- packages/server/src/app.module.ts
+++ packages/server/src/app.module.ts
@@ -1,6 +1,5 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
-import * as Joi from 'joi';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ExercisesModule } from './exercises/exercises.module';
@@ -10,19 +9,12 @@ import { AuthModule } from './auth/auth.module';
 @Module({
   imports: [
     ConfigModule.forRoot({
-      cache: true,
       envFilePath: [
         `.env.${process.env.NODE_ENV || 'development'}.local`,
         '.env',
         `.env.${process.env.NODE_ENV || 'development'}`,
       ],
-      validationSchema: Joi.object({
-        NODE_ENV: Joi.string()
-          .valid('development', 'production', 'test')
-          .default('development'),
-        PORT: Joi.number().default(4000),
-        DATABASE_URL: Joi.string().required(),
-      }),
+      cache: true,
       isGlobal: true,
     }),
     ExercisesModule,
