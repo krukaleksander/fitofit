969aaba5f9e5c4c50b6fbd1fb18bb108dd97e516
Aleksander Kruk <kruk.aleksander@hotmail.com>
2022-04-15 08:56:47 +0200

    test: with overrideProvider pass should return historical activity

--- packages/server/src/exercises/exercises.controller.spec.ts
+++ packages/server/src/exercises/exercises.controller.spec.ts
@@ -2,6 +2,59 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { ExercisesController } from './exercises.controller';
 import { ExercisesService } from './exercises.service';
 import { db as MOCKED_RESPONSE } from '../tempdb/db';
+
+const historicalResponseMock = {
+  totalCalories: 50665,
+  totalDuration: 2600,
+  caloriesToBurgers: 77,
+  activities: [
+    {
+      activityID: 1,
+      durationInMinutes: 500,
+      exerciseID: 2,
+      isDone: true,
+      name: 'Badbinton',
+      start: '2022-04-12T10:49:12.089Z',
+      userID: 10,
+    },
+    {
+      activityID: 2,
+      durationInMinutes: 500,
+      exerciseID: 2,
+      isDone: true,
+      name: 'Badbinton',
+      start: '2022-04-12T10:49:12.089Z',
+      userID: 10,
+    },
+    {
+      activityID: 3,
+      durationInMinutes: 500,
+      exerciseID: 2,
+      isDone: true,
+      name: 'Badbinton',
+      start: '2022-04-12T10:49:12.089Z',
+      userID: 10,
+    },
+    {
+      activityID: 4,
+      durationInMinutes: 500,
+      exerciseID: 2,
+      isDone: true,
+      name: 'Badbinton',
+      start: '2022-04-12T10:49:12.089Z',
+      userID: 10,
+    },
+    {
+      activityID: 5,
+      durationInMinutes: 600,
+      exerciseID: 2,
+      isDone: true,
+      name: 'Badbinton',
+      start: '2022-04-12T10:49:12.089Z',
+      userID: 10,
+    },
+  ],
+};
 describe('ExercisesController', () => {
   let controller: ExercisesController;
 
@@ -11,6 +64,9 @@ describe('ExercisesController', () => {
     }),
     addActivity: jest.fn((dto) => {
       return { status: 200, msg: 'Dodano zadanie', activity: dto };
+    }),
+    getHistoricalActivities: jest.fn(() => {
+      return historicalResponseMock;
     }),
   };
 
@@ -46,5 +102,10 @@ describe('ExercisesController', () => {
       msg: 'Dodano zadanie',
       activity,
     });
+  });
+  it('should return historical activity', () => {
+    expect(controller.getHistoricalActivities()).toEqual(
+      historicalResponseMock,
+    );
   });
 });
