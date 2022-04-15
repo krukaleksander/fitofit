afe8e75d92a2425d5b10c7e157da43d947e7f2b3
Aleksander Kruk <kruk.aleksander@hotmail.com>
2022-04-15 08:40:14 +0200

    test: pass with overrideProvider exercise controller to be defined

--- packages/server/src/exercises/exercises.controller.spec.ts
+++ packages/server/src/exercises/exercises.controller.spec.ts
@@ -1,13 +1,24 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { ExercisesController } from './exercises.controller';
+import { ExercisesService } from './exercises.service';
 
 describe('ExercisesController', () => {
   let controller: ExercisesController;
 
+  const mockExerciseService = {
+    getExercises: jest.fn((dto) => {
+      return { status: 200, msg: 'Dodano zadanie', activity: dto };
+    }),
+  };
+
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [ExercisesController],
-    }).compile();
+      providers: [ExercisesService],
+    })
+      .overrideProvider(ExercisesService)
+      .useValue(mockExerciseService)
+      .compile();
 
     controller = module.get<ExercisesController>(ExercisesController);
   });
